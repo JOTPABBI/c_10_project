36ce10c6d5ba1cc00089c6dd9ca6825c609467e3
JOTPABBI <86107597+JOTPABBI@users.noreply.github.com>
2021-07-12 19:13:19 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,16 @@
+var ship,ship1
+var sea,sea1
+function preload(){
+ship=loadanimation("ship-1.png","ship-2.png","ship-3.png","ship-4.png)
+sea1=loadanimation("sea.png")
+}
+
+function setup(){
+  createCanvas(600,500);
+  
+}
+
+function draw() {
+  background("blue");
+ 
+}
